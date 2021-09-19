9d7b6e7b353bab0108ee6d61aaa3050a514dcd50
Amanda Chen <chenamanda256@gmail.com>
2021-09-18 22:56:17 -0400

    improving landing page ui

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,21 +1,22 @@
 import React from "react";
 import "../landing.css";
+import logo from "../htn_logo.png";
+
 
 const Landing = () => {
   return (
     <div className="full-container">
-      <h1 className="header">Hello. Welcome to [name].</h1>
-      <p className="subheader">The new way to streamline planning.</p>
-      <p className="prompt">Enter your room code to start!</p>
-      <div className="entry">
-        <input
-          className="input-field"
-          type="text"
-          placeholder="Enter your room code here"
-        ></input>
-        <button className="submit" type="submit">
-          Submit
-        </button>
+      <div className="left-container">
+        <h1 className="header">Welcome to <br></br> Chatr Planr.</h1>
+        <p className="subheader">The new way to streamline planning.</p>
+        <p className="prompt">Enter your room code to start!</p>
+        <div className="entry">
+          <input className="input-field" type="text" placeholder="Enter your room code here"></input>
+          <button className="submit" type="submit">Submit</button>
+        </div>
+      </div>
+      <div className="right-container">
+        <img className="logo" src={logo} alt="calendar with speech bubbles" />
       </div>
     </div>
   );
