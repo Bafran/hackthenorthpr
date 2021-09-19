2022a23db5cb378d41276001e14489b17af2f787
Bafran <aachinoy@gmail.com>
2021-09-19 03:02:49 -0400

    landing route

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,18 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 import "../landing.css";
 import logo from "../htn_logo.png";
 
-
 const Landing = () => {
+  const [value, setValue] = useState("");
+  const history = useHistory();
+
   return (
     <div className="full-container">
       <div className="left-container">
-        <h1 className="header">Welcome to <br></br> Chatr Planr.</h1>
+        <h1 className="header">
+          Welcome to <br></br> Chatr Planr.
+        </h1>
         <p className="subheader">The new way to streamline planning.</p>
         <p className="prompt">Enter your room code to start!</p>
         <div className="entry">
-          <input className="input-field" type="text" placeholder="Enter your room code here"></input>
-          <button className="submit" type="submit">Submit</button>
+          <form
+            onSubmit={() => {
+              history.push(`/id/${value}`);
+            }}
+          >
+            <label>
+              <input
+                className="input-field"
+                type="text"
+                placeholder="Enter your room code here"
+                value={value}
+                onChange={(event) => {
+                  setValue(event.target.value);
+                }}
+              />
+            </label>
+            <button className="submit" type="submit">
+              Submit
+            </button>
+          </form>
         </div>
       </div>
       <div className="right-container">
